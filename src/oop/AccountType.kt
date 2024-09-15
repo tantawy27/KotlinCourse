ebcf93ac1bba5d8b0a9dbf731570dd8e14848f1c
tantawy27 <atantawy27101999@gmail.com>
2024-09-15 17:27:30 +0300

    Initial commit

---
+++ src/oop/AccountType.kt
@@ -0,0 +1,33 @@
+package oop
+
+fun main() {
+    println(AccountType.PLATINUM)
+    val accountTypeFromApi="Gold"
+
+
+    val accountType=AccountType.valueOf(accountTypeFromApi.uppercase())
+    println(accountType)
+
+
+    val user = UserLevel("ahmed",AccountType.PLATINUM)
+
+}
+
+
+
+
+
+enum class AccountType {
+    BRONZE,
+    SILVER,
+    GOLD,
+    PLATINUM
+}
+
+
+
+
+
+class UserLevel(var name:String,accountType: AccountType){
+
+}
