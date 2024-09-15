f2d47a60987548e08ad842f20ae61578a5dc6043
tantawy27 <atantawy27101999@gmail.com>
2024-09-15 17:11:43 +0300

    Initial commit

--- src/oop/Nested Classes.kt
+++ src/oop/Nested Classes.kt
@@ -5,13 +5,32 @@ fun main() {
     val obj2=Nested.Nested2()       // call second class
     obj2.info()
 
+
+    val obj3=Outer()
+    obj3.sayHi="Hi !"
+    val obj4=obj3.Nested3()
+    obj4.info()
+
+
 }
 
 
 class Nested{
 
-    class Nested2 {
+        class Nested2 {
         var name:String="this is nested class number 2"
+        fun info(){
+            println(name)
+        }
+    }
+
+}
+
+class Outer{
+var sayHi:String=""
+
+    inner class Nested3 {                         //"inner" keyword to access the Outer properties
+        var name:String="$sayHi this is nested class number 3"
         fun info(){
             println(name)
         }
