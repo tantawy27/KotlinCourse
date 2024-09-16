61cf26fcbd3e1ad8849e63d0c82c44d6b97e7f1d
tantawy27 <atantawy27101999@gmail.com>
2024-09-16 22:44:40 +0300

    ComponentN in a Data Class

--- src/oop/DataClass.kt
+++ src/oop/DataClass.kt
@@ -3,9 +3,16 @@ package oop
 fun main() {
     val dc1= DataClass("Ahmed",25)
     val dc2= DataClass("Mohammed",30)
-
+    val dc3=DataClass()
     println(dc1.myInfo())
     println(dc2.myInfo())
+    println(dc3)                 // print default parameters
+
+    println("==================================")
+
+    val p1 = DataClass("Ali",30)
+    println(p1.component1())
+    println(p1.component2())
 }
 
 
@@ -15,7 +22,7 @@ fun main() {
 
 
 //equals /hashcode / toString()
-data class DataClass(val name:String , val age:Int) {
+data class DataClass(val name:String="UnKnown" , val age:Int=0) {
 
     fun myInfo():String{
         return "my name is $name and iam $age Years Old"
