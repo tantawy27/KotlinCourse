a19f769275d2cb7a981fc4c4aa702fd460eb360d
tantawy27 <atantawy27101999@gmail.com>
2024-09-24 18:14:46 +0300

    DoWhileLoop

---
+++ src/loop/WhileLoop.kt
@@ -0,0 +1,16 @@
+package loop
+
+fun main() {
+//    var x = 0
+//    while (x < 10){
+//        println(x)
+//        x++
+//    }
+
+    var x = 0
+    do {
+        println(x)
+        x++
+    }
+    while (x < 10)
+}
