9fa2d29036128eac013213dd08afd6d43eaacad7
tantawy27 <atantawy27101999@gmail.com>
2024-10-02 18:53:30 +0300

    Anonymous

---
+++ src/Anonymous/A.kt
@@ -0,0 +1,27 @@
+package Anonymous
+
+open class A(val name:String) {
+    open fun printName() {
+        println("Hi $name")
+    }
+}
+
+
+fun main() {
+
+    val helloWorld = object {
+        val hello = "Hello"
+        val world = "World"
+        override fun toString() = "$hello $world"
+        val name = "Tamer"
+        fun printName() {
+            println("Hi $name")
+        }
+    }
+    println(helloWorld)
+    println(helloWorld.printName())
+
+
+//    val obj = A("Tamer")
+//    obj.printName()
+}
