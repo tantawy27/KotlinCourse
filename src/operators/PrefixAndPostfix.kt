23558f1a87daeb515e531665dd399dd25e0de9c4
tantawy27 <atantawy27101999@gmail.com>
2024-10-02 04:49:08 +0300

    ComparisonOperators

---
+++ src/operators/PrefixAndPostfix.kt
@@ -0,0 +1,21 @@
+package operators
+
+//Prefix and Postfix in Kotlin                          PreFix      PostFix
+//    ++	        Increment: increases value by 1	    ++a          a++
+//    --	        Decrement: decreases value by 1 	--a          a--
+
+
+fun main() {
+
+    var a = 6
+    var b = 0
+    println("a value is $a")
+    println("b value is $b")
+
+    //PreFix
+    b = ++a
+    println("a value is $a")
+    println("b value is $b")
+
+
+}
