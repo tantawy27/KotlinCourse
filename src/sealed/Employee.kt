78122365d51b2de37bc906d53c28d2bcd01f9e43
tantawy27 <atantawy27101999@gmail.com>
2024-10-02 06:09:53 +0300

    sealed class

---
+++ src/sealed/Employee.kt
@@ -0,0 +1,20 @@
+package sealed
+
+sealed class Employee
+
+
+    data class Manager(val name: String, val age: Int, val team: Int) : Employee()     //inheritance
+    class SeniorDev(val name: String, val age: Int, val projects: Int) : Employee()
+    object JuniorDev : Employee()
+
+
+
+
+
+
+
+
+
+
+
+
